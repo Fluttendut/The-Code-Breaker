336d7a10f00a29b077c117a04dc5007b2968b087
Ella Frost <ded_19@hotmail.com>
2021-09-22 10:13:02 +0200

    First

--- src/TheCodeBreaker.java
+++ src/TheCodeBreaker.java
@@ -1,3 +1,16 @@
 public class TheCodeBreaker
 {
+    public static void main(String[] args)
+    {
+
+        String alphabet = "abcdefghijklmnopqrstuvxuz";
+        int charIndex = alphabet.indexOf(alphabet) + 1;
+
+    }
+    public static char characterToIndex(char charIndex)
+    {
+        return 0;
+
+    }
 }
+
