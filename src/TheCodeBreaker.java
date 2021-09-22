1d959d6d3957e6d6ccdfd472034d0fcafdfd4e00
Ella Frost <ded_19@hotmail.com>
2021-09-22 14:45:45 +0200

    done!

--- src/TheCodeBreaker.java
+++ src/TheCodeBreaker.java
@@ -1,16 +1,46 @@
 public class TheCodeBreaker
 {
+    //this is a field
+    static String alphabet = "abcdefghijklmnopqrstuvwxuz";
+
+    //Because my main is static, everything else needs to be static too. Else it breaks.
     public static void main(String[] args)
     {
+        char input = 'b';
+        System.out.println(characterToIndex(input));
 
-        String alphabet = "abcdefghijklmnopqrstuvxuz";
-        int charIndex = alphabet.indexOf(alphabet) + 1;
+        indexToCharacter(5);
 
+        ceasarCharacterEncoder('o');
+        //String test = alphabet.substring(2, alphabet.length());
+        //System.out.println(test.substring(test.length() - 1));
+
+        //System.out.println(test);
     }
-    public static char characterToIndex(char charIndex)
+
+    public static int characterToIndex(char charIndex)
     {
-        return 0;
+        //altid +1 for index hos alphabet
+        return alphabet.indexOf(charIndex) + 1;
+    }
+
+    public static char indexToCharacter(int numberIndex)
+    {
+        char result = alphabet.charAt(numberIndex);
+        System.out.println(result);
+        return result;
+    }
+
+    // static class means, you can call that method without instanciating that class (without creating an object of that class)
+    //Everytime I pass a parameter I need to put a type + name in there.
+    public static void ceasarCharacterEncoder(char characterToCharacter)
+    {
+        int number = characterToIndex(characterToCharacter);
+
+        //I want to know why it can't be 3 and without -1
+        String test = alphabet.substring(number, number + 3);
+        System.out.println(test.substring(test.length() - 1));
+
 
     }
 }
-
